74c49581837354e13089ca02463043df9234ba5a
Barel Shraga <barel123133@gmail.com>
2023-06-24 17:33:32 +0300

    added backdrop to headers to make more it more readable

--- components/Content/index.tsx
+++ components/Content/index.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
 import UsePortableText from './UseRichText';
+import ContactMePage from './ContactMePage';
 import ContactForm from '../ContactForm';
 import AccordionQA from '../Accordion';
 import { urlFor } from '@/client';
-import ContactMePage from './ContactMePage';
 
 function Content({ route, contact }: { route: Route; contact?: ContactInfo }) {
 	const slug = route?.slug?.current;
@@ -11,22 +11,23 @@ function Content({ route, contact }: { route: Route; contact?: ContactInfo }) {
 	return (
 		<div className="content m-auto text-center">
 			{route?.image && (
-				<div className="background-image background-image-home w-full text-center -z-10 top-0 absolute overflow-hidden min-h-[900px]">
+				<div
+					className={`w-full text-center -z-10 top-0 absolute overflow-hidden min-h-[900px] background-image background-image-${
+						route.slug.current === '/' ? 'home' : route.slug.current
+					}`}
+				>
 					<Image
 						src={urlFor(route.image).url()}
 						alt="Office image"
 						width={2000}
 						height={2000}
 						loading="lazy"
-						className="w-full scale-[4] sm:scale-[3] md:scale-[2.5] lg:scale-[2]"
+						// className="w-full scale-[4] sm:scale-[3] md:scale-[2.5] lg:scale-[2] background-image"
+						className="w-full background-image"
 					/>
 				</div>
 			)}
-			<div
-				className={`text-slate-800 dark:text-slate-300 content-header normal-line-height text-center font-sans md:backdrop-blur-sm max-w-full my-20 mx-2 md:mt-28 text-slate-${
-					slug === '/' ? '50' : '800'
-				} m-auto`}
-			>
+			<div className="text-slate-800 backdrop-brightness-150 dark:backdrop-saturate-0 dark:backdrop-brightness-50 dark:text-slate-300 content-header normal-line-height text-center font-sans max-w-full my-20 mx-2 md:mt-28">
 				{route?.header && <UsePortableText value={route.header!} />}
 			</div>
 
