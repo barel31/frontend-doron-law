8ae97792d05030d269d3ed3b8590b7ecb960c868
barel31 <barel123133@gmail.com>
2024-04-07 00:53:03 +0300

    Refactor Header component to improve mobile navigation functionality

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -27,34 +27,36 @@ function Header({
   const ref = useRef<HTMLElement>(null);
   const params = useParams();
 
-  useEffect(() => {
-    if (show && mobile) {
-      document.body.style.overflow = 'hidden';
-      ref.current!.style.height = ref.current!.scrollHeight + 80 + 'px';
-    } else {
-      document.body.style.overflow = 'visible';
-      ref.current!.style.height = isTop ? '4rem' : '2.5rem';
-    }
+  const toggleNavBar = () => setShow((prevShow) => !prevShow);
+  const hideNavBar = () => setShow(false);
+  const getLogoWidth = () =>
+    isTop ? (mobile ? '185' : '220') : mobile ? '120' : '150';
 
-    const clickListener = (e: MouseEvent) => {
-      if (mobile && show && !ref.current?.contains(e.target as Node)) {
-        onNavClick();
+  useEffect(() => {
+    const adjustStyles = () => {
+      if (show && mobile) {
+        document.body.style.overflow = 'hidden';
+        ref.current!.style.height = `${ref.current!.scrollHeight + 80}px`;
+      } else {
+        document.body.style.overflow = 'visible';
+        ref.current!.style.height = isTop ? '4rem' : '2.5rem';
       }
     };
 
+    const clickListener = (e: MouseEvent) => {
+      if (mobile && show && !ref.current?.contains(e.target as Node)) {
+        hideNavBar();
+      }
+    };
+
+    adjustStyles();
     document.addEventListener('click', clickListener);
     return () => document.removeEventListener('click', clickListener);
   }, [mobile, show, isTop]);
 
-  const showNavBar = () => setShow(() => !show);
-  const onNavClick = () => setShow(() => false);
-
-  const logoWidth = isTop ? (mobile ? '185' : '220') : mobile ? '120' : '150';
-
   return (
     <header className="fixed z-10 top-0 w-full min-h-[2.5rem] bg-slate-200/90 dark:bg-slate-600/90">
       <ScrollLine />
-
       <nav
         ref={ref}
         className="flex justify-between w-[90%] lg:w-4/5 items-center m-auto my-1 md:my-3 h-[2.5rem] transition-[height]">
@@ -66,16 +68,16 @@ function Header({
             src={Logo}
             alt="Logo"
             priority
-            width={logoWidth}
-            height={logoWidth}
-            className={`self-start bg-slate-50/70 rounded-sm transition-all w-[${logoWidth}px] h-auto`}
+            width={getLogoWidth()}
+            height={getLogoWidth()}
+            className={`self-start bg-slate-50/70 rounded-sm transition-all w-[${getLogoWidth()}px] h-auto`}
           />
         </Link>
         <button
           className={`self-start md:hidden order-1 transition-all ${
             isTop && 'mt-3'
           }`}
-          onClick={showNavBar}
+          onClick={toggleNavBar}
           title="Navigation"
           type="button">
           <SolarHamburgerMenuBold className="w-10 m-auto" />
@@ -84,25 +86,21 @@ function Header({
           className={`navbar-links flex flex-col md:flex-row max-md:self-start justify-evenly md:min-w-[50%] max-md:basis-3/5 max-md:mt-3 ${
             (mobile && show) || !mobile ? 'visible' : 'invisible'
           }`}>
-          {routes.map((route: Route) => {
-            if (route.isChild) {
-              return null;
-            }
-
-            return (
+          {routes.map((route: Route) =>
+            route.isChild ? null : (
               <RouteLink
                 route={route}
-                onNavClick={onNavClick}
+                onNavClick={hideNavBar}
                 params={params}
                 key={route._id}
               />
-            );
-          })}
+            )
+          )}
         </div>
         <HeaderContact contact={contact} mobile={mobile} show={show} />
         <Link
           href="/"
-          onClick={onNavClick}
+          onClick={hideNavBar}
           className={`absolute bottom-1 right-1 md:self-center ${
             show && mobile ? 'visible' : 'invisible'
           }`}
