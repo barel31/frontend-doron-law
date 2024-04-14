1759670999a2d5ea3a8cb29f27e96dbb48df549c
barel31 <barel123133@gmail.com>
2024-04-14 12:06:20 +0300

    Update styling and class names in ContactMePage component

--- components/Content/ContactMePage.tsx
+++ components/Content/ContactMePage.tsx
@@ -9,7 +9,7 @@ import {
 
 function ContactMePage({ contact }: { contact: ContactInfo }) {
   return (
-    <div className="m-auto flex flex-col lg:flex-row justify-between gap-5 xl:w-3/4 lg:m-5 xl:m-auto">
+    <div className="m-auto flex flex-col lg:flex-row justify-between gap-5 xl:w-3/4 lg:m-5 xl:m-auto relative top-20">
       <div className="lg:w-1/3">
         <h1 className="text-2xl/relaxed text-slate-800 dark:text-slate-300">
           תופס יצירת קשר
@@ -24,27 +24,27 @@ function ContactMePage({ contact }: { contact: ContactInfo }) {
         </h1>
         <div className="bg-slate-300">
           <div className="grid grid-cols-2 grid-rows-2 gap-[1px]">
-            <div className="bg-slate-50 dark:bg-slate-700 dark:text-slate-50 p-5 lg:p-10 flex flex-col items-center justify-center min-w-52 min-h-52">
+            <div className="bg-slate-50 dark:bg-slate-700 dark:text-slate-50 p-5 lg:p-5 flex flex-col items-center justify-center min-w-54 min-h-52">
               <Fa6SolidAddressCard className="w-12" />
               <span>{contact?.address}</span>
             </div>
             <a
               href={`tel:${contact?.mobile}`}
-              className="bg-slate-50 dark:bg-slate-700 dark:text-slate-50 p-5 lg:p-10 flex flex-col items-center justify-center min-w-52 min-h-52"
+              className="bg-slate-50 dark:bg-slate-700 dark:text-slate-50 p-5 lg:p-5 flex flex-col items-center justify-center min-w-54 min-h-52"
               title="טלפון נייד">
               <OcticonDeviceMobile className="w-10" />
               <span>{contact?.mobile}</span>
             </a>
             <a
               href={`mailto:${contact?.email}`}
-              className="bg-slate-50 dark:bg-slate-700 dark:text-slate-50 p-5 lg:p-10 flex flex-col items-center justify-center min-w-52 min-h-52"
+              className="bg-slate-50 dark:bg-slate-700 dark:text-slate-50 p-5 lg:p-5 flex flex-col items-center justify-center min-w-54 min-h-52"
               title="אימייל">
               <MaterialSymbolsMailOutline className="w-12" />
               <span>{contact?.email}</span>
             </a>
             <a
               href={`tel:${contact?.phone}`}
-              className="bg-slate-50 dark:bg-slate-700 dark:text-slate-50 p-5 lg:p-10 flex flex-col items-center justify-center min-w-52 min-h-52"
+              className="bg-slate-50 dark:bg-slate-700 dark:text-slate-50 p-5 lg:p-5 flex flex-col items-center justify-center min-w-54 min-h-52"
               title="טלפון">
               <IcBaselinePhoneEnabled className="w-12" />
               <span>{contact?.phone}</span>
@@ -57,11 +57,7 @@ function ContactMePage({ contact }: { contact: ContactInfo }) {
           מפה
         </h1>
         <div>
-          <GoogleMapsEmbed
-            address={contact?.address}
-            // tabletDynamicRatio={0.9}
-            // mobileDynamicRatio={0.9}
-          />
+          <GoogleMapsEmbed address={contact?.address} />
         </div>
       </div>
     </div>
