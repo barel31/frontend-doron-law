ecc2306d8e937cf1e14cee594215ae87405f91d2
barel31 <barel123133@gmail.com>
2024-04-14 12:29:35 +0300

    Move manifest.ts, robots.txt, and sitemap.ts files to app folder

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 import { MetadataRoute } from 'next';
-import { getRoutes } from './client';
+import { getRoutes } from '../client';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const routes = await getRoutes;
