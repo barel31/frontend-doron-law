4fba31942bfa5161c18f5a642874f1a694f10994
barel31 <barel123133@gmail.com>
2024-05-12 10:32:52 +0300

    chore: Fix OcticonDeviceMobile icon to response to currentColor

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -74,7 +74,7 @@ const Carousel = ({ images }: { images: string[] }) => {
             drag="x"
             dragConstraints={{ left: 0, right: 0 }}
             dragElastic={1}
-            onDragEnd={(e, { offset, velocity }) => {
+            onDragEnd={(_, { offset, velocity }) => {
               const swipe = swipePower(offset.x, velocity.x);
 
               if (swipe < -swipeConfidenceThreshold) {
--- utils/icons.tsx
+++ utils/icons.tsx
@@ -102,7 +102,10 @@ export function IonIosSunny(props: SVGProps<SVGSVGElement>) {
 export function OcticonDeviceMobile(props: SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox="0 0 10 16" {...props}>
-      <path d="M9 0H1C.45 0 0 .45 0 1v14c0 .55.45 1 1 1h8c.55 0 1-.45 1-1V1c0-.55-.45-1-1-1zM5 15.3c-.72 0-1.3-.58-1.3-1.3c0-.72.58-1.3 1.3-1.3c.72 0 1.3.58 1.3 1.3c0 .72-.58 1.3-1.3 1.3zM9 12H1V2h8v10z"></path>
+      <path
+        fillRule="evenodd"
+        d="M9 0H1C.45 0 0 .45 0 1v14c0 .55.45 1 1 1h8c.55 0 1-.45 1-1V1c0-.55-.45-1-1-1zM5 15.3c-.72 0-1.3-.58-1.3-1.3c0-.72.58-1.3 1.3-1.3c.72 0 1.3.58 1.3 1.3c0 .72-.58 1.3-1.3 1.3zM9 12H1V2h8v10z"
+        fill="currentColor"></path>
     </svg>
   );
 }
