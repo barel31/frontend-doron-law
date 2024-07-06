1c998eb11ef68af662c620d752d74173d90d487c
barel31 <barel123133@gmail.com>
2024-07-06 14:31:16 +0300

    refactor: Split components in Footer.tsx

--- components/GoogleMapsEmbed.tsx
+++ components/GoogleMapsEmbed.tsx
@@ -2,7 +2,7 @@ export default function GoogleMapsEmbed({ address }: { address: string }) {
   return (
     <iframe
       title="Google Maps - Doron Hadad Law Office"
-      className="m-auto w-full h-full"
+      className="m-auto w-full h-full min-h-[300px]"
       loading="lazy"
       allowFullScreen
       src={`https://www.google.com/maps/embed/v1/place?q=${address}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_EMBED_KEY}`}></iframe>
