74cff8ed555e2fc1c976969f1433be14b29bcc8d
barel31 <barel123133@gmail.com>
2024-12-01 13:52:42 +0200

    chore: update card title to use h5 if no link provide

--- components/Card.tsx
+++ components/Card.tsx
@@ -10,8 +10,8 @@ const Card = ({ title, content, image, link }: CardProps) => {
 
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      {image && (
-        <Link href={link || ''} target="_blank">
+      {image && url ? (
+        <Link href={link || ''} target={link ? '_blank' : undefined}>
           <Image
             className="rounded-t-lg w-full object-cover"
             src={url}
@@ -20,19 +20,35 @@ const Card = ({ title, content, image, link }: CardProps) => {
             height={600}
           />
         </Link>
+      ) : (
+        image && (
+          <Image
+            className="rounded-t-lg w-full object-cover"
+            src={url}
+            alt={title}
+            width={500}
+            height={600}
+          />
+        )
       )}
       <div className="p-6">
-        <Link href={link || ''} target="_blank">
+        {link ? (
+          <Link href={link} target="_blank">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {title}
+            </h5>
+          </Link>
+        ) : (
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {title}
           </h5>
-        </Link>
+        )}
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
           {content}
         </p>
-        {isContentLong && (
+        {isContentLong && link && (
           <Link
-            href={link || ''}
+            href={link}
             target="_blank"
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             קרא עוד
