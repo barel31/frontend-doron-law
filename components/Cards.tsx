ec52b45aba60e4aac97325e78d65b680e960573e
barel31 <barel123133@gmail.com>
2024-12-01 13:04:33 +0200

    feat: add image to cards

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -2,7 +2,7 @@ import Card from './Card';
 
 const Cards = ({ cards }: { cards: CardProps[] }) => {
   return (
-    <div className='flex gap-4'>
+    <div className='flex gap-4 flex-wrap'>
       {cards.map((card, index) => (
         <Card key={index} {...card} />
       ))}
