1d127d09debef8ecbbefb08804a2de09e7eb9ad4
barel31 <barel123133@gmail.com>
2024-12-06 14:19:02 +0200

    feat: send contact me request to whatsapp and email

--- app/actions.ts
+++ app/actions.ts
@@ -1,16 +1,24 @@
 'use server';
 
 import sendWhatsAppMessage from '@/service/sendWhatsApp';
-// import sendMail from '@/service/sendMail';
+import sendMail from '@/service/sendMail';
 
-export const ContactFormAction = async (e: FormData): Promise<WhatsAppResponse> => {
-	const formData: ContactMeData = {
-		name: e.get('name')!.toString(),
-		tel: e.get('tel')!.toString(),
-		email: e.get('email')!.toString(),
-		message: e.get('message')?.toString(),
-	};
+export const ContactFormAction = async (
+  e: FormData
+): Promise<WhatsAppResponse | MailResponse> => {
+  const formData: ContactMeData = {
+    name: e.get('name')!.toString(),
+    tel: e.get('tel')!.toString(),
+    email: e.get('email')!.toString(),
+    message: e.get('message')?.toString(),
+  };
 
-	// return await sendMail(formData);
-	return await sendWhatsAppMessage(formData)
+  const mailResponse = await sendMail(formData);
+  const WhatsAppResponse = await sendWhatsAppMessage(formData);
+
+  if (mailResponse.err) {
+    return mailResponse;
+  }
+
+  return WhatsAppResponse;
 };
